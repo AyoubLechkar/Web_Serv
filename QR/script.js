f8152a4d4cd3bb80e1b7d8f37b1f21eec3296b2e
AyoubLechkar <lechayoub1@gmail.com>
2023-09-28 16:15:24 +0100

    qr png

--- QR/script.js
+++ QR/script.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const urlInput = document.getElementById('url');
     const generateBtn = document.getElementById('generate-btn');
     const qrcodeContainer = document.getElementById('qrcode');
-    const downloadPngLink = document.getElementById('download-png-link');
-    const downloadPdfLink = document.getElementById('download-pdf-link'); // Added this line
+    const downloadLink = document.getElementById('download-link');
 
     generateBtn.addEventListener('click', () => {
         const url = urlInput.value.trim();
@@ -23,19 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create a data URL for the QR code image
         const qrImageDataUrl = qrcodeContainer.querySelector('img').src;
 
-        // Set the download links' href and display them
-        downloadPngLink.href = qrImageDataUrl;
-        downloadPngLink.style.display = 'block';
+        // Set the download link's href and display it
+        downloadLink.href = qrImageDataUrl;
+        downloadLink.style.display = 'block';
+    });
 
-        // Create a PDF document with the QR code
-        const pdf = new jsPDF();
-        pdf.addImage(qrImageDataUrl, 'PNG', 10, 10, 80, 80);
-
-        // Save the PDF as a data URL
-        const pdfDataUrl = pdf.output('datauristring');
-
-        // Set the PDF download link's href and display it
-        downloadPdfLink.href = pdfDataUrl;
-        downloadPdfLink.style.display = 'block'; // Added this line
+    downloadLink.addEventListener('click', () => {
+        // Add 'download' attribute to initiate the download
+        downloadLink.setAttribute('download', 'qrcode.png');
     });
 });
